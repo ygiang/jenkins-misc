26fc3db6d1c5bc29099e9554e3670c647035c58a
Yung Giang <ygiang@gmail.com>
2021-07-30 10:00:10 -0400

    Create jenkins_secret_decrypt.groovy

---
+++ groovy/jenkins_secret_decrypt.groovy
@@ -0,0 +1,21 @@
+// run this in the jenkins script console to decrypt existing jenkins credentials
+
+import jenkins.*
+import jenkins.model.* 
+import hudson.*
+import hudson.model.*
+def jenkinsCredentials = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
+        com.cloudbees.plugins.credentials.Credentials.class,
+        Jenkins.instance,
+        null,
+        null
+);
+
+for(c in jenkinsCredentials) {
+  if(c instanceof com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey){
+    println(String.format("id=%s  desc=%s key=%s\n", c.id, c.description, c.privateKeySource.getPrivateKeys()))
+  }
+  if (c instanceof com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl){
+    println(String.format("id=%s  desc=%s user=%s pass=%s\n", c.id, c.description, c.username, c.password))
+  }
+}
